7205530fe85f41e5e298b1b04b3184e2e7b20ac7
joshua-roberts <jdroberts94@gmail.com>
2019-11-12 13:52:24 -0500

    refactor PDP constructor

--- src/main/java/gov/nist/csd/pm/pdp/PDP.java
+++ src/main/java/gov/nist/csd/pm/pdp/PDP.java
@@ -1,6 +1,7 @@
 package gov.nist.csd.pm.pdp;
 
 import gov.nist.csd.pm.epp.EPP;
+import gov.nist.csd.pm.epp.functions.FunctionExecutor;
 import gov.nist.csd.pm.exceptions.PMException;
 import gov.nist.csd.pm.pap.PAP;
 import gov.nist.csd.pm.pdp.services.AnalyticsService;
@@ -18,23 +19,22 @@ public class PDP {
     private AnalyticsService    analyticsService;
     private ObligationsService  obligationsService;
 
-    public PDP(PAP pap) throws PMException {
+    /**
+     * Create a new PDP instance given a Policy Administration Point and an optional set of FunctionExecutors to be
+     * used by the EPP.
+     * @param pap the Policy Administration Point that the PDP will use to change the graph.
+     * @param functionExecutors an optional list of external functions to be used by the PDP's EPP at runtime.
+     * @throws PMException if there is an error initializing the EPP.
+     */
+    public PDP(PAP pap, FunctionExecutor ... functionExecutors) throws PMException {
         this.pap = pap;
-        this.epp = new EPP(this);
+        this.epp = new EPP(this, functionExecutors);
 
+        // initialize services
         this.graphService = new GraphService(this.pap, this.epp);
         this.prohibitionsService = new ProhibitionsService(this.pap, this.epp);
         this.analyticsService = new AnalyticsService(this.pap, this.epp);
         this.obligationsService = new ObligationsService(this.pap, this.epp);
-    }
-
-    public PDP(PAP pap, EPP epp) {
-        this.pap = pap;
-        this.epp = epp;
-    }
-
-    public void setEPP(EPP epp) {
-        this.epp = epp;
     }
 
     public EPP getEPP() {
