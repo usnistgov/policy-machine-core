48584ade959e79ff5b86826aff3a79577310b3c3
joshua-roberts <jdroberts94@gmail.com>
2020-04-03 15:02:48 -0400

    fix prohibition container bug

--- src/main/java/gov/nist/csd/pm/pdp/decider/PReviewDecider.java
+++ src/main/java/gov/nist/csd/pm/pdp/decider/PReviewDecider.java
@@ -88,7 +88,7 @@ public class PReviewDecider implements Decider {
         TargetContext targetCtx = processTargetDAG(target, userCtx);
 
         // resolve the permissions
-        return resolvePermissions(userCtx, targetCtx);
+        return resolvePermissions(userCtx, targetCtx, target);
     }
 
     @Override
@@ -126,7 +126,7 @@ public class PReviewDecider implements Decider {
                 // run dfs on the object
                 TargetContext targetCtx = processTargetDAG(object, userCtx);
 
-                HashSet<String> permissions = resolvePermissions(userCtx, targetCtx);
+                HashSet<String> permissions = resolvePermissions(userCtx, targetCtx, object);
                 results.put(object, permissions);
             }
         }
@@ -146,7 +146,7 @@ public class PReviewDecider implements Decider {
         return acl;
     }
 
-    private HashSet<String> resolvePermissions(UserContext userContext, TargetContext targetCtx) {
+    private HashSet<String> resolvePermissions(UserContext userContext, TargetContext targetCtx, String target) {
         Map<String, Set<String>> pcMap = targetCtx.getPcSet();
 
         HashSet<String> inter = new HashSet<>();
@@ -175,7 +175,7 @@ public class PReviewDecider implements Decider {
         }
 
         // remove any prohibited operations
-        Set<String> denied = resolveProhibitions(userContext, targetCtx);
+        Set<String> denied = resolveProhibitions(userContext, targetCtx, target);
         inter.removeAll(denied);
 
         // if the permission set includes *, ignore all other permissions
@@ -187,7 +187,7 @@ public class PReviewDecider implements Decider {
         return inter;
     }
 
-    private Set<String> resolveProhibitions(UserContext userCtx, TargetContext targetCtx) {
+    private Set<String> resolveProhibitions(UserContext userCtx, TargetContext targetCtx, String target) {
         Set<String> denied = new HashSet<>();
 
         Set<Prohibition> prohibitions = userCtx.getProhibitions();
@@ -199,6 +199,18 @@ public class PReviewDecider implements Decider {
 
             boolean addOps = false;
             for (String contName : containers.keySet()) {
+                if (target.equals(contName)) {
+                    addOps = false;
+                    if (inter) {
+                        // if the target is a container and the prohibition evaluates the intersection
+                        // the whole prohibition is not satisfied
+                        break;
+                    } else {
+                        // continue checking the remaining conditions
+                        continue;
+                    }
+                }
+
                 boolean isComplement = containers.get(contName);
                 if (!isComplement && reachedTargets.contains(contName) || isComplement && !reachedTargets.contains(contName)) {
                     addOps = true;
@@ -244,11 +256,7 @@ public class PReviewDecider implements Decider {
 
         Visitor visitor = node -> {
             // mark the node as reached, to be used for resolving prohibitions
-            // do not add the target of the decision because it may be a container in a prohibition
-            // and prohibition containers only apply to nodes inside the container, not the container itself
-            if (!node.getName().equals(target)) {
-                reachedTargets.add(node.getName());
-            }
+            reachedTargets.add(node.getName());
 
             Map<String, Set<String>> nodeCtx = visitedNodes.getOrDefault(node.getName(), new HashMap<>());
             if (nodeCtx.isEmpty()) {
