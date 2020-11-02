32e42def35bfe7a959bc61e5b3736e2623d22967
dyiop <akashworkmail@gmail.com>
2020-11-02 14:54:59 -0500

    Finished up DAC configuration and delegation method:  - added the consent obligation  - the delegation method happens entirely from delegator user context.  - added adder/remover for function executors in EPP

--- src/main/java/gov/nist/csd/pm/epp/EPP.java
+++ src/main/java/gov/nist/csd/pm/epp/EPP.java
@@ -35,11 +35,19 @@ public class EPP {
         this.functionEvaluator = new FunctionEvaluator();
         if (eppOptions != null) {
             for (FunctionExecutor executor : eppOptions.getExecutors()) {
-                this.functionEvaluator.addFunctionExecutor(executor);
+                addFunctionExecutor(executor);
             }
         }
     }
-    
+
+    public void addFunctionExecutor(FunctionExecutor executor) {
+        this.functionEvaluator.addFunctionExecutor(executor);
+    }
+
+    public void removeFunctionExecutor(FunctionExecutor executor) {
+        this.functionEvaluator.removeFunctionExecutor(executor);
+    }
+
     public PDP getPDP() {
         return pdp;
     }
