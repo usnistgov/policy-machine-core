117af6ae8f3fa7cf5e94a789a96c7f88b48d5f77
joshua-roberts <joshua.roberts@nist.gov>
2023-06-08 16:46:07 -0400

    add json property value test

--- src/test/java/gov/nist/csd/pm/pap/memory/MemoryPAPTest.java
+++ src/test/java/gov/nist/csd/pm/pap/memory/MemoryPAPTest.java
@@ -1,11 +1,16 @@
 package gov.nist.csd.pm.pap.memory;
 
+import com.google.gson.Gson;
 import gov.nist.csd.pm.pap.PAP;
 import gov.nist.csd.pm.policy.exceptions.PMException;
+import gov.nist.csd.pm.policy.model.graph.nodes.Node;
+import gov.nist.csd.pm.policy.model.graph.nodes.Properties;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Arrays;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class MemoryPAPTest {
 
@@ -21,5 +26,19 @@ class MemoryPAPTest {
         pap.graph().createPolicyClass("pc1");
         pap.commit();
         assertTrue(pap.graph().nodeExists("pc1"));
+    }
+
+    @Test
+    void testJsonAsPropertyValue() throws PMException {
+        PAP pap = new PAP(new MemoryPolicyStore());
+        pap.graph().createPolicyClass("name", Properties.toProperties("test", "{\"12\": \"34\"}", "a", "[\"1\", \"2\"]"));
+        Node name = pap.graph().getNode("name");
+        Map<String, String> properties = name.getProperties();
+        String test = properties.get("test");
+        Map m = new Gson().fromJson(test, Map.class);
+        assertEquals("34", m.get("12"));
+
+        String json = String.valueOf(properties.get("a"));
+        assertArrayEquals(new String[]{"1", "2"}, new Gson().fromJson(json, String[].class));
     }
 }
