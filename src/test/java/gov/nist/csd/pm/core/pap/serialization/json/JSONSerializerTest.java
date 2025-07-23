eb2c404dbde17b980de1d1193018eb2270ee99f2
joshua-roberts <joshua.roberts@nist.gov>
2025-07-23 08:04:01 -0400

    Fix json schema and serializer (#202)

--- src/test/java/gov/nist/csd/pm/core/pap/serialization/json/JSONSerializerTest.java
+++ src/test/java/gov/nist/csd/pm/core/pap/serialization/json/JSONSerializerTest.java
@@ -1,14 +1,16 @@
 package gov.nist.csd.pm.core.pap.serialization.json;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import com.google.gson.Gson;
 import gov.nist.csd.pm.core.common.exception.PMException;
-import gov.nist.csd.pm.core.common.graph.node.NodeType;
 import gov.nist.csd.pm.core.impl.memory.pap.MemoryPAP;
-import gov.nist.csd.pm.core.impl.memory.pap.store.MemoryGraphStore;
-import gov.nist.csd.pm.core.impl.memory.pap.store.MemoryPolicy;
-import gov.nist.csd.pm.core.impl.memory.pap.store.MemoryPolicyStore;
 import gov.nist.csd.pm.core.util.PolicyEquals;
 import gov.nist.csd.pm.core.util.SamplePolicy;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 class JSONSerializerTest {
@@ -27,10 +29,21 @@ class JSONSerializerTest {
     }
 
     @Test
-    void testWithAdminPolicyDoesNotError() throws PMException {
+    void testSerializeDoesNotIncludeNullFields() throws PMException {
+        MemoryPAP pap = new MemoryPAP();
+        long pc1 = pap.modify().graph().createPolicyClass("pc1");
+        pap.modify().graph().createObjectAttribute("oa1", List.of(pc1));
+        String serialize = pap.serialize(new JSONSerializer());
+        Map map = new Gson().fromJson(serialize, Map.class);
+        assertNull(map.get("prohibitions"));
+        assertNull(map.get("obligations"));
+        assertNull(map.get("operations"));
+        assertNull(map.get("routines"));
 
-        MemoryPolicyStore memoryPolicyStore = new MemoryPolicyStore();
-        memoryPolicyStore.graph().createNode(-1, "PM_ADMIN", NodeType.PC);
+        Map graph = (Map) map.get("graph");
+        assertNull(graph.get("uas"));
+        assertNull(graph.get("users"));
+        assertNull(graph.get("objects"));
+        assertNull(((Map)((List) graph.get("oas")).getFirst()).get("associations"));
     }
-
 }
