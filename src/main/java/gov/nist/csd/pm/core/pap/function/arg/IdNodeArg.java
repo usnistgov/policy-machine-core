41bab4e4d6e68947dcf35e59b16dd0d5f2f8019c
joshua-roberts <joshua.roberts@nist.gov>
2026-01-12 07:51:23 -0500

    add NOdeType and NodeArgs - fix errors

--- src/main/java/gov/nist/csd/pm/core/pap/function/arg/IdNodeArg.java
+++ src/main/java/gov/nist/csd/pm/core/pap/function/arg/IdNodeArg.java
@@ -2,4 +2,7 @@ package gov.nist.csd.pm.core.pap.function.arg;
 
 public final class IdNodeArg extends NodeArg<Long> {
 
+    public IdNodeArg(Long value) {
+        super(value);
+    }
 }
--- src/main/java/gov/nist/csd/pm/core/pap/function/arg/NameNodeArg.java
+++ src/main/java/gov/nist/csd/pm/core/pap/function/arg/NameNodeArg.java
@@ -2,4 +2,7 @@ package gov.nist.csd.pm.core.pap.function.arg;
 
 public final class NameNodeArg extends NodeArg<String> {
 
+    public NameNodeArg(String value) {
+        super(value);
+    }
 }
