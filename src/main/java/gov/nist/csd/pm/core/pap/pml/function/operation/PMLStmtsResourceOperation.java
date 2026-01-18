e9ea043269d305743267435342eb81928e2544fa
joshua-roberts <joshua.roberts@nist.gov>
2026-01-18 06:59:16 -0500

    before moving routines to operations and ensuring unique names

---
+++ src/main/java/gov/nist/csd/pm/core/pap/pml/function/operation/PMLStmtsResourceOperation.java
@@ -0,0 +1,71 @@
+package gov.nist.csd.pm.core.pap.pml.function.operation;
+
+import gov.nist.csd.pm.core.common.exception.PMException;
+import gov.nist.csd.pm.core.pap.function.arg.Args;
+import gov.nist.csd.pm.core.pap.function.arg.FormalParameter;
+import gov.nist.csd.pm.core.pap.function.arg.type.Type;
+import gov.nist.csd.pm.core.pap.pml.context.ExecutionContext;
+import gov.nist.csd.pm.core.pap.pml.statement.PMLStatementBlock;
+import gov.nist.csd.pm.core.pap.pml.statement.PMLStatementSerializable;
+import gov.nist.csd.pm.core.pap.query.PolicyQuery;
+import java.util.List;
+import java.util.Objects;
+
+public class PMLStmtsResourceOperation<T> extends PMLResourceOperation<T> implements PMLStatementSerializable {
+
+    private final PMLStatementBlock body;
+
+    public PMLStmtsResourceOperation(String name,
+                                  Type<T> returnType,
+                                  List<FormalParameter<?>> formalParameters,
+                                  PMLStatementBlock body) {
+        super(name, returnType, formalParameters);
+        this.body = body;
+    }
+
+    public PMLStatementBlock getBody() {
+        return body;
+    }
+
+    @Override
+    protected T execute(PolicyQuery query, Args args) throws PMException {
+        ExecutionContext ctx = getCtx();
+
+        Object result = ctx.executeOperationStatements(this.body.getStmts(), args);
+
+        return getReturnType().cast(result);
+    }
+
+    @Override
+    public String toFormattedString(int indentLevel) {
+        return String.format(
+            "%s%s",
+            getSignature().toFormattedString(indentLevel),
+            body.toFormattedString(indentLevel)
+        );
+    }
+
+    @Override
+    public String toString() {
+        return toFormattedString(0);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof PMLStmtsResourceOperation<?> that)) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        return Objects.equals(body, that.body);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), body);
+    }
+}
