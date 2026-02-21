deac4307cc47de959256a0eb7f026e3c232cba62
joshua-roberts <joshua.roberts@nist.gov>
2026-02-21 14:05:50 -0500

    update query

--- src/main/java/gov/nist/csd/pm/core/impl/grpc/client/GrpcQueryPDP.java
+++ src/main/java/gov/nist/csd/pm/core/impl/grpc/client/GrpcQueryPDP.java
@@ -2,25 +2,54 @@ package gov.nist.csd.pm.core.impl.grpc.client;
 
 import static gov.nist.csd.pm.core.impl.grpc.client.GrpcHeaders.buildHeaders;
 
+import gov.nist.csd.pm.core.pap.query.AccessQuery;
+import gov.nist.csd.pm.core.pap.query.GraphQuery;
+import gov.nist.csd.pm.core.pap.query.ObligationsQuery;
+import gov.nist.csd.pm.core.pap.query.OperationsQuery;
+import gov.nist.csd.pm.core.pap.query.PolicyQuery;
+import gov.nist.csd.pm.core.pap.query.ProhibitionsQuery;
+import gov.nist.csd.pm.core.pap.query.model.context.UserContext;
+import gov.nist.csd.pm.core.pdp.query.SelfAccessQuery;
 import gov.nist.csd.pm.proto.v1.pdp.query.PolicyQueryServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.stub.MetadataUtils;
 
-public class GrpcQueryPDP {
+public class GrpcQueryPDP implements PolicyQuery {
 
-    private final ManagedChannel managedChannel;
-    private final String user;
-    private final String process;
+    private GrpcPolicyQuerier grpcPolicyQuerier;
 
     public GrpcQueryPDP(ManagedChannel managedChannel, String user, String process) {
-        this.managedChannel = managedChannel;
-        this.user = user;
-        this.process = process;
-    }
-
-    public GrpcPolicyQuerier query() {
-        return new GrpcPolicyQuerier(
+        this.grpcPolicyQuerier = new GrpcPolicyQuerier(
             PolicyQueryServiceGrpc.newBlockingStub(managedChannel)
                 .withInterceptors(MetadataUtils.newAttachHeadersInterceptor(buildHeaders(user, process))));
+    }
+
+    @Override
+    public AccessQuery access() {
+        return grpcPolicyQuerier.access();
+    }
+
+    public SelfAccessQuery selfAccess(UserContext userCtx) {
+        return grpcPolicyQuerier.selfAccess(userCtx);
+    }
+
+    @Override
+    public GraphQuery graph() {
+        return grpcPolicyQuerier.graph();
+    }
+
+    @Override
+    public ProhibitionsQuery prohibitions() {
+        return grpcPolicyQuerier.prohibitions();
+    }
+
+    @Override
+    public ObligationsQuery obligations() {
+        return grpcPolicyQuerier.obligations();
+    }
+
+    @Override
+    public OperationsQuery operations() {
+        return grpcPolicyQuerier.operations();
     }
 }
