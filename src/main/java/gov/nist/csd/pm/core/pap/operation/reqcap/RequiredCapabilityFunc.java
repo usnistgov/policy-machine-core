7cc6b615354c871099d84bab8ec6667ef7c3e417
joshua-roberts <joshua.roberts@nist.gov>
2026-05-13 14:24:12 -0400

    remove unnecessary RequiredCapabilityFunc interface

--- src/main/java/gov/nist/csd/pm/core/pap/operation/reqcap/RequiredCapabilityFunc.java
+++ src/main/java/gov/nist/csd/pm/core/pap/operation/reqcap/RequiredCapabilityFunc.java
@@ -1,28 +1,23 @@
 package gov.nist.csd.pm.core.pap.operation.reqcap;
 
+import static gov.nist.csd.pm.core.pap.operation.arg.type.BasicTypes.STRING_TYPE;
+
 import gov.nist.csd.pm.core.common.exception.PMException;
 import gov.nist.csd.pm.core.pap.PAP;
+import gov.nist.csd.pm.core.pap.operation.ResourceOperation;
 import gov.nist.csd.pm.core.pap.operation.arg.Args;
+import gov.nist.csd.pm.core.pap.query.PolicyQuery;
 import gov.nist.csd.pm.core.pap.query.model.context.UserContext;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
-public class RequiredCapabilityFunc extends RequiredCapability {
+public abstract class RequiredCapabilityFunc extends RequiredCapability {
 
-    private final RequiredCapabilityFuncExecutor funcExecutor;
-
-    public RequiredCapabilityFunc(RequiredCapabilityFuncExecutor func) {
+    public RequiredCapabilityFunc() {
         super(new ArrayList<>());
-        this.funcExecutor = func;
     }
 
     @Override
-    public boolean isSatisfied(PAP pap, UserContext userCtx, Args args) throws PMException {
-        return funcExecutor.isSatisfied(pap, userCtx, args);
-    }
-
-    @FunctionalInterface
-    public interface RequiredCapabilityFuncExecutor extends Serializable {
-        boolean isSatisfied(PAP pap, UserContext userCtx, Args args);
-    }
+    public abstract boolean isSatisfied(PAP pap, UserContext userCtx, Args args) throws PMException;
 }
--- src/main/java/gov/nist/csd/pm/core/pap/pml/operation/PMLRequiredCapabilityFunc.java
+++ src/main/java/gov/nist/csd/pm/core/pap/pml/operation/PMLRequiredCapabilityFunc.java
@@ -15,7 +15,6 @@ public class PMLRequiredCapabilityFunc extends RequiredCapabilityFunc implements
     private PMLStatementBlock statementBlock;
 
     public PMLRequiredCapabilityFunc(PMLStatementBlock statementBlock) {
-        super(new PMLRequiredCapabilityFuncExecutor(statementBlock));
         this.statementBlock = statementBlock;
     }
 
@@ -24,30 +23,21 @@ public class PMLRequiredCapabilityFunc extends RequiredCapabilityFunc implements
         return String.format("@ReqCap(%s)", statementBlock.toFormattedString(indentLevel));
     }
 
-    static class PMLRequiredCapabilityFuncExecutor implements RequiredCapabilityFuncExecutor {
+    @Override
+    public boolean isSatisfied(PAP pap, UserContext userCtx, Args args) {
+        try {
+            ExecutionContext execCtx = pap.buildExecutionContext(userCtx);
 
-        private final PMLStatementBlock block;
-
-        public PMLRequiredCapabilityFuncExecutor(PMLStatementBlock block) {
-            this.block = block;
-        }
-
-        @Override
-        public boolean isSatisfied(PAP pap, UserContext userCtx, Args args) {
-            try {
-                ExecutionContext execCtx = pap.buildExecutionContext(userCtx);
-
-                // add args to execution context since they aren't passed to execute
-                Map<String, Object> map = args.toMap();
-                for (Map.Entry<String, Object> e : map.entrySet()) {
-                    execCtx.scope().addVariable(e.getKey(), e.getValue());
-                }
-
-                block.execute(execCtx, pap);
-                return true;
-            } catch (PMException e) {
-                return false;
+            // add args to execution context since they aren't passed to execute
+            Map<String, Object> map = args.toMap();
+            for (Map.Entry<String, Object> e : map.entrySet()) {
+                execCtx.scope().addVariable(e.getKey(), e.getValue());
             }
+
+            statementBlock.execute(execCtx, pap);
+            return true;
+        } catch (PMException e) {
+            return false;
         }
     }
 }
