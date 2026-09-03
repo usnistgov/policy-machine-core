9c84900abec4c789e891670f87755a42953ac5c1
joshua-roberts <joshua.roberts@nist.gov>
2026-09-03 15:13:13 -0400

    add license headers and fix neo4j license (#245)
    * add neo4j license statement
    
    * update license stmt
    
    * add nist license headers
    
    * update code metadata
    
    * add exception line to root license file and update pom.xml

--- neo4j/src/main/java/gov/nist/ngac/pm/core/neo4j/embedded/pap/Neo4jEmbeddedPAP.java
+++ neo4j/src/main/java/gov/nist/ngac/pm/core/neo4j/embedded/pap/Neo4jEmbeddedPAP.java
@@ -1,3 +1,23 @@
+/*
+ * This Software (Policy Machine) is being made available as a public service by the
+ * National Institute of Standards and Technology (NIST), an Agency of the United
+ * States Department of Commerce. This software was developed in part by employees of
+ * NIST and in part by NIST contractors. Copyright in portions of this software that
+ * were developed by NIST contractors has been licensed or assigned to NIST. Pursuant
+ * to Title 17 United States Code Section 105, works of NIST employees are not
+ * subject to copyright protection in the United States. However, NIST may hold
+ * international copyright in software created by its employees and domestic
+ * copyright (or licensing rights) in portions of software that were assigned or
+ * licensed to NIST.
+ *
+ * This file is part of the policy-machine-core-neo4j module, which compiles against
+ * and embeds org.neo4j:neo4j (GPLv3, Community Edition). As a combined work, this
+ * module is distributed under the GNU General Public License v3.0, not the CC BY 4.0
+ * license used elsewhere in this repository. See neo4j/LICENSE for the full text.
+ */
+
+// This file is part of the policy-machine-core-neo4j module, which links against
+// org.neo4j:neo4j (GPLv3) and is distributed under GPLv3. See neo4j/LICENSE.
 package gov.nist.ngac.pm.core.neo4j.embedded.pap;
 
 import gov.nist.ngac.pm.core.common.exception.PMException;
